b31323ab4d5620ff026b4b6d1674641ca366dde9
Loc Truong <loc@kiuglobal.com>
2017-11-10 00:01:08 +0700

    songs browse view

--- client/src/components/Songs.vue
+++ client/src/components/Songs.vue
@@ -9,14 +9,33 @@
                 <b-link @click="navigateTo({name: 'songs-create'})">
                   <icon 
                     name="plus" 
-                    scale="1"></icon>
+                    scale="2" />
                 </b-link>
               </b-col>
             </b-row>
           </div>
           <div class="card-text">
-            <div v-for="song in songs" :key="song.id">
-              {{song.title}}
+            <div class="song mr-4" v-for="song in songs" :key="song.id">
+              <b-row>
+                <b-col md="6" class="text-center">
+                  <div class="song-title">{{song.title}}</div>
+                  <div class="song-artist">{{song.artist}}</div>
+                  <div class="song-genre">{{song.genre}}</div>
+                  <b-button 
+                    class="mt-4" 
+                    variant="primary" 
+                    @click="navigateTo({
+                      name: 'song',
+                      params: {
+                        songId: song.id
+                      }
+                    })"
+                  >View</b-button>
+                </b-col>
+                <b-col md="6">
+                  <img class="album-image" :src="song.albumImageUrl" />
+                </b-col>
+              </b-row>
             </div>
           </div>
         </b-card>
@@ -45,4 +64,18 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.song {
+  padding: 20px;
+  height: 330px;
+  overflow: hidden;
+}
+.song-title {
+  font-size: 28px;
+}
+.song-artist {
+  font-size: 22px;
+}
+.album-image {
+  margin: 10px;
+}
 </style>
